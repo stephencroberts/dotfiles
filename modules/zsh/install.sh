b06b98bb8e20f8928c366e03dc72fe8f251aefdf
Stephen Roberts <stephenroberts@gmail.com>
2022-01-02 21:26:30 -0700

    feat(zsh): add config files

--- modules/zsh/install.sh
+++ modules/zsh/install.sh
@@ -1,9 +1,15 @@
 if [ ! -e "${ZDOTDIR:-$HOME}/.zprezto" ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+  # Symlink defaults
+  for rcfile in $(ls "${ZDOTDIR:-$HOME}"/.zprezto/runcoms | grep -v README.md)
+  do
+    ln -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/$rcfile" \
+      "${ZDOTDIR:-$HOME}/.$rcfile"
+  done
 fi
 
-# TODO: Move config files to dotfiles and symlink
-for rcfile in $(ls "${ZDOTDIR:-$HOME}"/.zprezto/runcoms | grep -v README.md); do
-  # link_file "${ZDOTDIR:-$HOME}/.zprezto/runcoms/$rcfile"
-  ln -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/$rcfile" "${ZDOTDIR:-$HOME}/.$rcfile"
-done
+# Symlink overrides
+link_file "$DOTFILES/modules/zsh/.zpreztorc"
+link_file "$DOTFILES/modules/zsh/.zprofile"
+link_file "$DOTFILES/modules/zsh/.zshrc"

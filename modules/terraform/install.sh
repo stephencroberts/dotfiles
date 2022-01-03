f25ae6cd8babb8ca33315ac760414398675a33fa
Stephen Roberts <stephenroberts@gmail.com>
2022-01-03 12:54:07 -0700

    feat(terraform): add terraform module

---
+++ modules/terraform/install.sh
@@ -0,0 +1,10 @@
+type asdf >/dev/null || {
+  log_error "Please install the asdf module!"
+  return 0
+}
+
+asdf plugin list | grep terraform >/dev/null \
+  || asdf plugin-add \
+     terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf install terraform latest
+asdf global terraform latest

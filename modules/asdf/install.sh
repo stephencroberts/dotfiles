ab14b591bd06bf46e2b285e737c7cbb1ffb2ec46
Stephen Roberts <stephenroberts@gmail.com>
2022-01-04 12:46:43 -0700

    feat(asdf): add alpine support

--- modules/asdf/install.sh
+++ modules/asdf/install.sh
@@ -1,5 +1,7 @@
 if [ "$1" = macos ]; then
   brew list asdf >/dev/null || brew install asdf
+elif [ "$1" = alpine ]; then
+  [ -e ~/.asdf ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 else
   log_error "$1 is not supported!"
 fi
--- modules/asdf/source.sh
+++ modules/asdf/source.sh
@@ -1,1 +1,11 @@
-type asdf >/dev/null && . $(brew --prefix asdf)/asdf.sh
+if type brew >/dev/null 2>&1 && brew --prefix asdf >/dev/null 2>&1; then
+  . "$(brew --prefix asdf)/asdf.sh"
+  echo "$0" | grep bash >/dev/null 2>&1 \
+    && "$(brew --prefix asdf)/etc/bash_completion.d/asdf.bash"
+elif [ -e "$HOME/.asdf" ]; then
+  . "$HOME/.asdf/asdf.sh"
+  echo "$0" | grep bash >/dev/null 2>&1 && . "$HOME/.asdf/completions/asdf.bash"
+else
+  log_error "Failed to find asdf!"
+  return 0
+fi

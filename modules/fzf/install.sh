8c80edf4b217745c8cc73079fd1960b52b151a01
Stephen Roberts <stephen.roberts1@davita.com>
2022-01-06 09:11:50 -0700

    feat(fzf): add debian support

--- modules/fzf/install.sh
+++ modules/fzf/install.sh
@@ -1,10 +1,23 @@
 if [ "$1" = macos ]; then
+
   if ! brew list fzf >/dev/null; then
     brew install fzf
     "$(brew --prefix)/opt/fzf/install"
   fi
+
 elif [ "$1" = alpine ]; then
   type fzf >/dev/null || apk add fzf
+
+elif [ "$1" = debian ]; then
+
+  if ! type fzf >/dev/null; then
+
+    # Try installing with apt with fallback to git
+    apt-get install -qq fzf || {
+      git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+      "$HOME/.fzf/install"
+    }
+  fi
 else
   log_error "$1 is not supported!"
 fi
--- modules/fzf/source.sh
+++ modules/fzf/source.sh
@@ -1,5 +1,9 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# fzf auto-completions do not work in bourne shell
+echo "$0" | grep '\(^\|.*\/\)sh$' >/dev/null 2>&1 && return 0
+
+echo "$0" | grep "zsh" >/dev/null 2>&1 && [ -f ~/.fzf.zsh ] && . ~/.fzf.zsh
+echo "$0" | grep "bash" >/dev/null 2>&1 && [ -f ~/.fzf.bash ] && . ~/.fzf.bash
 
 # Use rg for fzf
-type rg >/dev/null && type fzf >/dev/null \
+type rg >/dev/null 2>&1 && type fzf >/dev/null 2>&1 \
   && export FZF_DEFAULT_COMMAND='rg --files --hidden --glob=!.git'

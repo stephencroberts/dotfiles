3304216bc0dd76ac0c3925a8735f9ee8d0cb53b8
Stephen Roberts <stephen.roberts1@davita.com>
2022-01-06 10:41:49 -0700

    feat(dev-extras): add debian support

--- modules/dev-extras/install.sh
+++ modules/dev-extras/install.sh
@@ -2,6 +2,8 @@ if [ "$1" = macos ]; then
   brew list jq >/dev/null || brew install jq
 elif [ "$1" = alpine ]; then
   type jq >/dev/null || apk add jq
+elif [ "$1" = debian ]; then
+  type jq >/dev/null || apt-get -qq install jq
 else
   log_error "$1 is not supported!"
   return 0

91c473662ef4215ceb0cafa838494702920faa0b
Stephen Roberts <stephen.roberts1@davita.com>
2022-01-07 16:21:18 -0700

    fix(asdf): install unzip required for installing plugins

--- modules/asdf/install.sh
+++ modules/asdf/install.sh
@@ -1,6 +1,10 @@
 if [ "$1" = macos ]; then
   brew_install asdf
-elif [ "$1" = alpine ] || [ "$1" = debian ]; then
+elif [ "$1" = alpine ]; then
+  apk_add unzip
+  [ -e ~/.asdf ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+elif [ "$1" = debian ]; then
+  apt_install unzip
   [ -e ~/.asdf ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 else
   log_error "$1 is not supported!"

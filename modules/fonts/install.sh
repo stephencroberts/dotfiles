0232a1d4f443d7635062756982d6a5cf50530427
Stephen Roberts <stephenroberts@gmail.com>
2022-05-04 08:49:53 -0600

    feat: install FiraCodeiScript font and use for iTerm

--- modules/fonts/install.sh
+++ modules/fonts/install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ ! -d FiraCodeiScript ]; then
+  git clone https://github.com/kencrocken/FiraCodeiScript.git
+fi
+
 if [ "$1" = macos ]; then
   brew tap | grep homebrew/cask-fonnts >/dev/null \
     || brew tap homebrew/cask-fonts
   brew_cask_install font-fira-code
+  cp FiraCodeiScript/*.ttf $HOME/Library/Fonts
 else
   log_error "$1 is not supported!"
   return 0

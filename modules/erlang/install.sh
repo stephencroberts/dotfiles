0bd54bff196ffbed39fbe814e452506c07d1e606
Stephen Roberts <stephenroberts@gmail.com>
2022-11-19 10:09:21 -0700

    feat(erlang): install with asdf and configure rebar

--- modules/erlang/install.sh
+++ modules/erlang/install.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-if [ "$1" = macos ]; then
-  brew_install erlang
-else
-  type erl >/dev/null || {
-    log_error "$1 is not supported!"
-    return 0
-  }
-fi
+type asdf >/dev/null || {
+  log_error "Please install the asdf module!"
+  return 0
+}
+
+asdf plugin list | grep erlang >/dev/null \
+  || asdf plugin add erlang
+asdf install erlang latest
+asdf global erlang latest
+
+asdf plugin list | grep rebar >/dev/null \
+  || asdf plugin add rebar
+asdf install rebar latest
+asdf global rebar latest
+
+# Hack to fix issue with terminal output being garbled after using rebar3
+# https://github.com/erlang/rebar3/issues/2472#issuecomment-762270455
+rebar3 local install
+rebar3 local upgrade
+
+link_file "$DOTFILES/modules/erlang/rebar.config" \
+  "$HOME/.config/rebar3/rebar.config"
---
+++ modules/erlang/source.sh
@@ -0,0 +1,1 @@
+export PATH="$HOME/.cache/rebar3/bin:$PATH"

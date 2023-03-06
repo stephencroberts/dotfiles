4076b122cd93b45581be9d31b002c93edc4cc920
Stephen Roberts <stephenroberts@gmail.com>
2023-03-06 11:54:39 -0700

    feat(yabai): add yabai/skhd config

--- modules/yabai/install.sh
+++ modules/yabai/install.sh
@@ -2,9 +2,15 @@
 
 if [ "$1" = macos ]; then
   brew_install koekeishiya/formulae/yabai
+  brew services start yabai
+  brew install koekeishiya/formulae/skhd
+  brew services start skhd
 else
   type yabai >/dev/null || {
     log_error "$1 is not supported!"
     return 0
   }
 fi
+
+link_file "$DOTFILES/modules/yabai/.yabairc"
+link_file "$DOTFILES/modules/yabai/.skhdrc"

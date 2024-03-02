509ed62b8b51dc48da2ff7d73fc8848c63662a2e
Stephen Roberts <stephenroberts@gmail.com>
2024-03-02 09:48:39 -0700

    feat(golang): add new module for golang

---
+++ modules/golang/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+type asdf >/dev/null || {
+  log_error "Please install the asdf module!"
+  return 0
+}
+
+asdf plugin list | grep golang >/dev/null \
+  || asdf plugin add golang
+asdf install golang latest
+asdf global golang latest

74e03e617ffe977130894ab891bb1d653d6134fe
Stephen Roberts <sroberts@edg.io>
2024-03-04 15:27:29 -0700

    feat(apps): add new apps and remove docker

--- modules/apps/install.sh
+++ modules/apps/install.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 if [ "$1" = macos ]; then
+  brew_install 1password
+  brew_install 1password-cli
   # mas is a cli for the App Store
   # https://github.com/mas-cli/mas
-  brew_install 1password
   brew_install mas
-  brew_cask_install docker
+  brew_cask_install arc
+  brew_cask_install clickup
+  brew_cask_install elgato-control-center
   brew_cask_install postman
+  brew_cask_install utm
   brew_cask_install visual-studio-code
 
-  mas list | grep "Evernote" >/dev/null|| mas install 406056744 # Evernote
+  mas list | grep "MeetingBar" >/dev/null|| mas install 1532419400 # MeetingBar
   mas list | grep "Slack" >/dev/null|| mas install 803453959 # Slack
+  mas list | grep "Spark" >/dev/null|| mas install 1176895641 # Spark
 
   if ! brew list --cask iterm2 >/dev/null; then
     brew_cask_install iterm2

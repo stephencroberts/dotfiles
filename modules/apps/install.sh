3ce60ab00c56b7d1d7a6c425c564bca3422e3813
Stephen Roberts <stephenroberts@gmail.com>
2024-03-05 20:10:17 -0700

    feat(apps): install op for debian

--- modules/apps/install.sh
+++ modules/apps/install.sh
@@ -29,6 +29,35 @@ if [ "$1" = macos ]; then
   fi
 
   link_file "$DOTFILES/modules/apps/.iterm2"
+elif [ "$1" = debian ]; then
+  # if ! type 1password >/dev/null; then
+  #   # sudo apt --fix-broken install?
+  #   apt_install gnupg2
+  #   curl -O https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb
+  #   $maybe_sudo dpkg -i 1password-latest.deb
+  #   rm 1password-latest.deb
+  # fi
+
+  if ! type op >/dev/null; then
+    # Install 1password-cli
+    # https://developer.1password.com/docs/cli/get-started/
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc \
+      | $maybe_sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" \
+      | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol \
+      | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc \
+      | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update
+    sudo apt install 1password-cli
+
+    # Add account and login
+    op account add
+    eval $(op signin)
+  fi
 else
   log_error "$1 is not supported!"
   return 0

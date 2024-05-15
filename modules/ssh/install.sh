6334754f9a8cc153152ddf2ca891c52641914fda
Stephen Roberts <sroberts@edg.io>
2024-05-15 13:22:21 +0000

    feat(ssh): use ssh auth for sudo on debian

--- modules/ssh/install.sh
+++ modules/ssh/install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 if [ "$1" = macos ]; then
-  brew_install autossh
+	brew_install autossh
+elif [ "$1" = debian ]; then
+	apt_install libpam-ssh-agent-auth
+	if [ ! -e /etc/sudoers.d/pam-ssh-agent-auth ]; then
+		echo 'Defaults    env_keep += "SSH_AUTH_SOCK"' >pam-ssh-agent-auth
+		$maybe_sudo chmod 440 pam-ssh-agent-auth
+		$maybe_sudo chown root:root pam-ssh-agent-auth
+		$maybe_sudo mv pam-ssh-agent-auth /etc/sudoers.d/
+
+		$maybe_sudo sed -i '/PAM/a \\nauth    sufficient  pam_ssh_agent_auth.so file=~/.ssh/authorized_keys' /etc/pam.d/sudo
+	fi
 fi
 
 mkdir -p "$HOME/.ssh"

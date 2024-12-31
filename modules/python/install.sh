6274906ad7c5cfd77f7747f69d25e18841877811
Stephen Roberts <sroberts@edg.io>
2024-12-31 10:25:12 -0700

    feat(python): add python module

---
+++ modules/python/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+type asdf >/dev/null || {
+  log_error "Please install the asdf module!"
+  return 0
+}
+
+asdf plugin list | grep python >/dev/null || asdf plugin-add python
+asdf install python latest
+asdf global python latest

5c8dd108b238587c342012d3600ce3a673117786
Stephen Roberts <stephenroberts@gmail.com>
2025-01-09 15:52:06 -0700

    feat(dotnet): add dotnet module

---
+++ modules/dotnet/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+type asdf >/dev/null || {
+  log_error "Please install the asdf module!"
+  return 0
+}
+
+asdf plugin list | grep dotnet >/dev/null || asdf plugin add dotnet
+asdf install dotnet latest
+asdf global dotnet latest

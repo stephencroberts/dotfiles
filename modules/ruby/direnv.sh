671feaa338a0c08c3ae003f8da5a4e90a624ee77
Stephen Roberts <stephenroberts@gmail.com>
2025-03-10 17:22:55 -0600

    feat(ruby): integrate ruby gems with gtags

---
+++ modules/ruby/direnv.sh
@@ -0,0 +1,8 @@
+gems=$(gem environment | grep -- "- INSTALLATION DIRECTORY" | awk '{print $4}')/gems
+if [ -e Gemfile ]; then
+	if [ -z "$GTAGSLIBPATH" ]; then
+		export GTAGSLIBPATH="$gems"
+	else
+		export GTAGSLIBPATH="$GTAGSLIBPATH:$gems"
+	fi
+fi
---
+++ modules/ruby/source.sh
@@ -0,0 +1,24 @@
+# Wrapper for bundle that generates gtags in the gems directory
+# Works in conjunction with direnv (./direnv.sh) to load the library
+bundle() {
+	command bundle "$@"
+
+	arg1="$1"
+	# All of the commands and aliases from the docs that affect packages
+	set -- add install remove update
+
+	for c in "$@"; do
+		if [ "$arg1" = "$c" ]; then
+			# Imporant! Unset args.
+			set --
+			wd=$(pwd)
+			# Go into the gems directory
+			cd "$(gem environment | grep -- "- INSTALLATION DIRECTORY" | awk '{print $4}')/gems" || return
+
+			echo "Generating gtags in $PWD"
+			find . -type f -name "*.rb" -print | gtags --file -
+
+			cd "$wd" || return
+		fi
+	done
+}

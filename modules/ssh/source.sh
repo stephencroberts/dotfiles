61d8629293decfc6333bd113ad3ed35346b997ab
Stephen Roberts <stephenroberts@gmail.com>
2025-03-27 16:06:28 -0600

    feat(ssh): add config to make sudo work with ssh auth on a remote machine while also using gpg

---
+++ modules/ssh/source.sh
@@ -0,0 +1,6 @@
+# When using gpg with ssh modules, ensure that the ssh module loads first and
+# the sudo alias will be set with the forwarded SSH_AUTH_SOCK instead of the one
+# set by gpg
+#
+# shellcheck disable=SC2139
+alias sudo="SSH_AUTH_SOCK=$SSH_AUTH_SOCK sudo"

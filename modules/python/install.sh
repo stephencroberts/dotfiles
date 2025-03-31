cdcc0c53bb5aa48487f1f3cdc0aa82c202b0776e
Stephen Roberts <stephenroberts@gmail.com>
2025-03-31 16:43:08 -0600

    fix(python; debian): fix python installation on debian

--- modules/python/install.sh
+++ modules/python/install.sh
@@ -5,6 +5,17 @@ type asdf >/dev/null || {
 	return 0
 }
 
+if [ "$1" = debian ]; then
+	apt_install libbz2-dev
+	apt_install libffi-dev
+	apt_install liblzma-dev
+	apt_install libreadline-dev
+	apt_install libsqlite3-dev
+	apt_install libssl-dev
+	apt_install tk-dev
+	apt_install zlib1g-dev
+fi
+
 asdf plugin list | grep python >/dev/null || asdf plugin add python
 asdf install python latest || true
 asdf set -u python latest

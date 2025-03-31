922b21338b5d3a0dd77a8c8a5e2120f2ef3e6de3
Stephen Roberts <stephenroberts@gmail.com>
2025-03-31 16:42:42 -0600

    feat(vpn): fix some issues getting the vpn working on ubuntu

--- modules/vpn/install.sh
+++ modules/vpn/install.sh
@@ -23,69 +23,22 @@ get_vpn_config() {
 if [ "$1" = macos ]; then
 	brew_install openconnect
 	brew_install vpn-slice
-
-	mkdir -p "$HOME/.vpn"
-	touch "$HOME/.vpn/routes"
-
-	if ! cat "$DOTFILES/.local" | grep "VPN_" 2>&1 >/dev/null; then
-		get_vpn_config
-		printf -- "\nexport VPN_ACCOUNT=\"%s\"\nexport VPN_VAULT=\"%s\"\nexport VPN_SECRET=\"%s\"" \
-			"$account" "$vault" "$secret" >>"$DOTFILES/.local"
-	fi
-
 elif [ "$1" = debian ]; then
 	apt_install openconnect
-
-	if [ ! -d "/etc/systemd/system/openconnect.service" ]; then
-		get_vpn_config
-		cat >openconnect.service <<EOF
-[Unit]
-Description=OpenConnect service
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/etc/openconnect/connect.sh
-Restart=always
-RestartSec=30
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-		cat >connect.sh <<EOF
-#!/bin/sh
-
-set -e
-
-username=\$(op --account "${account}" read "op://${vault}/${secret}/username")
-password=\$(op --account "${account}" read "op://${vault}/${secret}/password")
-authgroup=\$(op --account "${account}" read "op://${vault}/${secret}/authgroup")
-website=\$(op --account "${account}" read "op://${vault}/${secret}/website")
-otp=\$(op --account "${account}" item get "${secret}" --otp --vault "${vault}")
-
-printf -- "%s\n%s\n" "\$password" "\$otp" \\
-  | openconnect --user="\$username" \\
-    --authgroup="\$authgroup" \\
-    --script=/usr/share/vpnc-scripts/vpnc-script-fix \\
-    --csd-wrapper=/usr/libexec/openconnect/csd-post.sh \\
-    --os=linux-64 "\$website" \\
-    --non-inter \\
-    --passwd-on-stdin
-EOF
-		$maybe_sudo mv openconnect.service /etc/systemd/system/openconnect.service
-		$maybe_sudo mkdir -p /etc/openconnect
-		$maybe_sudo mv connect.sh /etc/openconnect/connect.sh
-		$maybe_sudo chmod +x /etc/openconnect/connect.sh
-		curl -fsSLO https://gitlab.com/openconnect/vpnc-scripts/-/raw/master/vpnc-script
-		$maybe_sudo chmod +x vpnc-script
-		$maybe_sudo mv vpnc-script /usr/share/vpnc-scripts/vpnc-script-fix
-		$maybe_sudo systemctl daemon-reload
-		$maybe_sudo systemctl start openconnect
-	fi
+	apt_install python3-full
+	sudo pip3 install "vpn-slice[dnspython,setproctitle]"
 else
 	type openconnect >/dev/null || {
 		log_error "$1 is not supported!"
 		return 0
 	}
+fi
+
+mkdir -p "$HOME/.vpn"
+touch "$HOME/.vpn/routes"
+
+if ! grep "VPN_" <"$DOTFILES/.local" >/dev/null 2>&1; then
+	get_vpn_config
+	printf -- "\nexport VPN_ACCOUNT=\"%s\"\nexport VPN_VAULT=\"%s\"\nexport VPN_SECRET=\"%s\"" \
+		"$account" "$vault" "$secret" >>"$DOTFILES/.local"
 fi
